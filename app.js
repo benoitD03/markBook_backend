beaf1eab2aa3ecf69bb0299dba7d5f76425c0870
benoitD03 <bens.92@hotmail.fr>
2021-09-30 09:17:12 +0200

    modify app.js

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use((req, res, next) => {
     next();
   });
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/users', userRoutes);
 app.use('/api/books', bookRoutes);
